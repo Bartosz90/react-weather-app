22ca947d1325228a56234a9925af29b684176463
Bartek Krupa <krupa1.bk@gmail.com>
2019-07-23 19:36:22 +0200

    Updates

---
+++ precache-manifest.91f3c7b01414a8fac00c9e9e7cfab931.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90d9a7d467cc90df8f7200a571a0289f",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "88c8a752d90f8b994814",
+    "url": "/react-weather-app/static/css/main.d0c59bc4.chunk.css"
+  },
+  {
+    "revision": "d2d82d68c7df91bb776f",
+    "url": "/react-weather-app/static/js/2.e703e8cc.chunk.js"
+  },
+  {
+    "revision": "88c8a752d90f8b994814",
+    "url": "/react-weather-app/static/js/main.d0bed6bb.chunk.js"
+  },
+  {
+    "revision": "e736fc5812ad96aa92b3",
+    "url": "/react-weather-app/static/js/runtime~main.7e96bc80.js"
+  }
+]);
